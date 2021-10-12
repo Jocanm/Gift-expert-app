925aed5702debd6e768ee5d4a0befa4fbb839812
Jose Angarita <amjose09@gmail.com>
2021-10-11 21:51:31 -0500

    Proyecto inicializado

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GiftExpertApp = () => {
+    return (
+        <div>
+            <h2>GiftExpertApp</h2>
+            <hr />
+        </div>
+    )
+}
+
+export default GiftExpertApp
