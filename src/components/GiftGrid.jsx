36ad72510978dd3cca5e616c18484fef49cdc408
Jose Angarita <amjose09@gmail.com>
2021-10-16 22:25:36 -0500

    Creando un custom hook para mostrar que los datos se están cargando

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -2,30 +2,36 @@ import React, { useState, useEffect } from 'react'
 import GiftGridItem from './GiftGridItem'
 import { nanoid } from 'nanoid'
 import { getGifs } from '../helpers/apiGif.js'
+import useFetchGifs from '../hooks/useFetchGifs'
 
 const GiftGrid = ({ category }) => {
 
-    const [images, setImages] = useState([])
+    // const [images, setImages] = useState([])
 
-    useEffect(() => {
-        getGifs(category)
-        .then(gifs => setImages(gifs))
-        .catch((err)=>{console.log("error")})
-    }, [category])
+    const {data,loading} = useFetchGifs();
+
+    // useEffect(() => {
+    //     getGifs(category)
+    //     .then(gifs => setImages(gifs))
+    //     .catch((err)=>{console.log("error")})
+    // }, [category])
 
     
 
     return (
         <>
             <h3 className="font-bold text-2xl py-6"> {category} </h3>
-            <div className="card-grid">
+
+            { loading? "Cargando...":"Datos cargados" }
+
+            {/* <div className="card-grid">
                 {
                     images.map(e => (
                         // Esto siginfica que le pasamos como propiedades al componente todos los llave-valor del objeto que estamos iterando con el .map()
                         <GiftGridItem key={nanoid()} {...e} />
                     ))
                 }
-            </div>
+            </div> */}
         </>
     )
 }
